a25de1931ca801c8700b97feb71275795ee1a602
sailakshmi <sailakshmi@codegnan.com>
2026-02-16 13:32:04 +0530

    random-password

---
+++ javascript/accessing.js
@@ -0,0 +1,18 @@
+
+    function srinu(){
+        alert("hellllooooo evryoneee!!!!!!!!!!")
+        // console.log("herlloo")
+    }
+    // srinu()
+
+
+
+    function callingElement(){
+        let v=document.getElementById("mahesh").value
+        let z=document.getElementById("email").value
+        console.log(v)
+        console.log(z)
+    }
+   
+
+
